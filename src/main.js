e5b36e55e01ff585090deb2b4037384905704717
Morgan Boirot <m.boirot@gmail.com>
2020-01-12 16:21:12 +0100

    Creation du la vue restaurant

--- src/main.js
+++ src/main.js
@@ -1,8 +1,42 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+import VueRouter from 'vue-router'
+import Restaurants from "./components/Restaurants.vue";
+//import RestaurantDetail from "./components/RestaurantDetail.vue";
+import HelloWorld from "./components/HelloWorld.vue";
+import vuetify from './plugins/vuetify';
 
+// configs...
+Vue.use(VueMaterial)
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+// config du router :
+const router = new VueRouter({
+  routes:[
+    { 
+      path: '/', 
+      component: HelloWorld, 
+      props:{
+        msg:" site des restaurants"
+      } 
+    },
+    { 
+      path: '/restaurants', 
+      component: Restaurants 
+    },
+    
+  ],
+  mode:'history'
+});
 
 new Vue({
-  render: h => h(App),
+  router,
+  vuetify,
+
+  // si router pas de rendu de composant
+  render: h => h(App)
 }).$mount('#app')
